a893f2c908b1aa7f478fa77f0203aab1e303a649
Jean-Baptiste Mayer <jean-baptiste.mayer@m4x.org>
2015-07-06 22:18:27 +0100

    Import of babl and gegl subprojects

--- MainWeb/main.cpp
+++ MainWeb/main.cpp
@@ -9,6 +9,7 @@
 #include <Wt/WLineEdit>
 #include <Wt/WPushButton>
 #include <Wt/WText>
+#include <gegl.h>
 
 // c++0x only, for std::bind
 // #include <functional>
@@ -94,17 +95,23 @@ WApplication *createApplication(const WEnvironment& env)
 
 int main(int argc, char **argv)
 {
-  /*
-   * Your main method may set up some shared resources, but should then
-   * start the server application (FastCGI or httpd) that starts listening
-   * for requests, and handles all of the application life cycles.
-   *
-   * The last argument to WRun specifies the function that will instantiate
-   * new application objects. That function is executed when a new user surfs
-   * to the Wt application, and after the library has negotiated browser
-   * support. The function should return a newly instantiated application
-   * object.
-   */
-  return WRun(argc, argv, &createApplication);
+    gegl_init(&argc, &argv);
+    /* license for this application, needed by fractal-explorer */
+    g_object_set (gegl_config (),
+	    "application-license", "GPL3",
+	    NULL);
+
+    /*
+     * Your main method may set up some shared resources, but should then
+     * start the server application (FastCGI or httpd) that starts listening
+     * for requests, and handles all of the application life cycles.
+     *
+     * The last argument to WRun specifies the function that will instantiate
+     * new application objects. That function is executed when a new user surfs
+     * to the Wt application, and after the library has negotiated browser
+     * support. The function should return a newly instantiated application
+     * object.
+     */
+    return WRun(argc, argv, &createApplication);
 }
 
---
+++ MainWeb/start.sh
@@ -0,0 +1,1 @@
+LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/jibee/Coding/Deratiseur/3rdParty/lib/ ./mainweb --docroot ../dependancies/wt/ --http-address=localhost --http-port=8080
